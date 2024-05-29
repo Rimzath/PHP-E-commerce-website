99769c7c8536d763ba96e5384943c0a918a1d214
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2024-05-29 14:21:45 +0530

    html-login, signup
    creating login and signup pages in html and css

--- conn.php
+++ conn.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "rmzh";
+
+
+$conn = new mysqli($server, $username, $password, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
 </body>
 </html>
