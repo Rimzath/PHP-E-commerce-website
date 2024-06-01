8753c424f961402a0b3182d6c6a0aab733fd4145
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2024-06-01 14:10:18 +0530

    created files

---
+++ rmzh.sql
@@ -0,0 +1,8 @@
+-- Dumping data for table `users`
+
+
+INSERT INTO `signup` (`id`, `name`, `username`, `password`, `user_type`) VALUES
+(1, 'user', 'uname', '1234', 'customer'),
+(2, 'uoc', 'uoc', 'uoc', 'customer'),
+(3, 'admin', 'admin', '1234', 'admin');
+COMMIT;
